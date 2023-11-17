c572ad12ca5db9f7f58151708c96c08f7607882c
George Bilias <georgebiliasgr@gmail.com>
2023-11-17 17:07:12 +0200

    gg

--- script.js
+++ script.js
@@ -18,6 +18,19 @@ function validateForm(event) {
         alert('Password must have at least one uppercase letter, one lowercase letter, one number, and be 8 characters long.');
         event.preventDefault();
     }
+    // Get the current date
+    var currentDate = new Date();
+
+    // Get the year, month, and day
+    var currentYear = currentDate.getFullYear();
+    var currentMonth = currentDate.getMonth() + 1; // Months are zero-based
+    var currentDay = currentDate.getDate();
+    // Format the current date in the yyyy-mm-dd format
+    var formattedCurrentDate = currentYear + '-' + (currentMonth < 10 ? '0' : '') + currentMonth + '-' + (currentDay < 10 ? '0' : '') + currentDay;
+    // Set the max attribute of the birthdate input to the current date
+    document.getElementById('birthdate').max = formattedCurrentDate;
+    document.getElementById('birthdate').min = '1903-01-01';
+    
 }
 
 function isValidPassword(password) {
