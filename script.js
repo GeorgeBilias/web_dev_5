e7d5416f005587883119fad631479461d25f8d4b
George Bilias <georgebiliasgr@gmail.com>
2023-11-17 18:01:09 +0200

    gg

--- script.js
+++ script.js
@@ -18,18 +18,85 @@ function validateForm(event) {
         alert('Password must have at least one uppercase letter, one lowercase letter, one number, and be 8 characters long.');
         event.preventDefault();
     }
-    // Get the current date
-    var currentDate = new Date();
 
-    // Get the year, month, and day
-    var currentYear = currentDate.getFullYear();
-    var currentMonth = currentDate.getMonth() + 1; // Months are zero-based
-    var currentDay = currentDate.getDate();
-    // Format the current date in the yyyy-mm-dd format
-    var formattedCurrentDate = currentYear + '-' + (currentMonth < 10 ? '0' : '') + currentMonth + '-' + (currentDay < 10 ? '0' : '') + currentDay;
-    // Set the max attribute of the birthdate input to the current date
-    document.getElementById('birthdate').max = formattedCurrentDate;
-    document.getElementById('birthdate').min = '1903-01-01';
+
+    // Get the birthdate input element
+    var birthdateInput = document.getElementById('birthdate');
+
+    // Set the min and max values
+    birthdateInput.min = '1900-01-01';
+    birthdateInput.max = new Date().toISOString().split('T')[0];
+
+    // Get the birthdate input value
+    var birthdate = birthdateInput.value;
+
+    // Convert the value to a Date object
+    var birthdateDate = new Date(birthdate);
+
+    // Convert the min and max values to Date objects
+    var minDate = new Date(birthdateInput.min);
+    var maxDate = new Date(birthdateInput.max);
+
+    // If the birthdate is less than the min date or greater than the max date
+    if (birthdateDate < minDate || birthdateDate > maxDate) {
+        // Prevent the form from being submitted
+        event.preventDefault();
+        // Alert the user
+        alert('Birthdate must be between ' + minDate.getFullYear() + ' and ' + maxDate.toISOString().split('T')[0] + '.');
+    }
+
+
+    // Get the address number and postal code input values
+    var phone_number = document.getElementById('phone').value.toString().length;
+    var postalCode = document.getElementById('postal_code').value.toString().length;
+
+    console.log('Phone number:', phone_number);
+    console.log('Postal code:', postalCode);
+
+    
+    if (phone_number!=10) {
+        
+        event.preventDefault();
+
+        // Alert the user
+        alert('Phone number must be 10 digits long.');
+    }
+
+    if (postalCode!=5) {
+        
+        event.preventDefault();
+
+        // Alert the user
+        alert('Postal code must be 5 digits long.');
+    }
+
+
+
+    // Get the checkbox elements
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    // Check if at least one checkbox is checked
+    var isAtLeastOneChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+
+    // If not
+    if (!isAtLeastOneChecked) {
+        // Prevent the form from being submitted
+        event.preventDefault();
+
+        // Alert the user
+        alert('Please select at least one contact method.');
+    }
+
+
+
+
+
+
+
+
+
+
+
     
 }
 
